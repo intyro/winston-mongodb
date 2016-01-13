70e33e3f1115cd511763551408dbd3bf59ebbeb4
Jokes7 <moath.khozyem@yahoo.com>
2016-01-13 15:41:25 +0200

    init commit

---
+++ package.js
@@ -0,0 +1,18 @@
+
+Package.describe({
+  name: 'jokes7:winston-mongodb',
+  summary: 'Meteor wrapper for the winston-mongodb npm package.',
+  version: '0.0.2',
+  git: 'https://github.com/lbergnehr/winston-mongodb.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('0.9.0');
+  api.export('MongoDB', 'server');
+  api.addFiles(['winston-mongodb.js'], 'server');
+});
+
+Npm.depends({
+  "winston": "0.7.3",
+  "winston-mongodb": "0.5.2"
+});
